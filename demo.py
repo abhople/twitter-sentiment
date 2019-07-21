82d1744f9bd2361ad18ebe97ce2f15bb8af357b3
abhople <anupam.bhople@gmail.com>
2019-07-20 22:26:43 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,19 +1,19 @@
 from textblob import TextBlob
 import tweepy
 
-consumer_key = 'qHUfehDpzdSvTIesjst8iDkc3'
-consumer_secret = '4ljEjAZfVFYQX3mablhkNlGJ46qQ8ilQbNZZLsYQssstR1g996'
+consumer_key = '' #<<your key>>
+consumer_secret = '' #<<your key>>
 
-accessToken = '56479932-J65KpbpVJkpSvLdBgwBeSXVbgODm7SypVsO62we09'
-accessSecret = 'Z39xUnCgVDlzOtuJlD3jfaRuD496bjM4O51mAkE96MTHS'
+accessToken = '' #<<your key>>
+accessSecret = '' #<<your key>>
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(accessToken, accessSecret)
 
 api = tweepy.API(auth)
 
-# public_tweets = api.user_timeline('iamsrk')
 public_tweets = api.search('hate')
+
 for tweet in public_tweets:
     print(tweet.text)
     analysis = TextBlob(tweet.text)
